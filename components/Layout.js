add1ac8981dfba1aba66889a657c75262dd3fced
petksks <peter.sedberg@gmail.com>
2023-03-21 11:48:43 +0100

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            backgroundColor: {
+                'primary': '#374151',
+                'secondary': '#374151',
+                'accent': '#000000',
+                'brand-bg': '#FCE44D',
+            },
+            textColor: {
+                'primary': '#374151',
+                'secondary': '#374151',
+                'accent': '#000000',
+            },
+            fontFamily: {
+                'brand': ['Comfortaa', 'sans-serif'],
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
